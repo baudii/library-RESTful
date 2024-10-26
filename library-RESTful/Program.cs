35dc8355c632168317be67b9460b631487e82bef
yanerobot <screwyuguys@gmail.com>
2024-10-26 11:02:14 +0300

    Added docker support. Now app can unwarp and function inside of the docker container

--- library-RESTful/Program.cs
+++ library-RESTful/Program.cs
@@ -1,22 +1,7 @@
 using library_RESTful.Data;
 using Microsoft.EntityFrameworkCore;
-using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
-
-string? docker_env = Environment.GetEnvironmentVariable("IS_DOCKER");
-string libConnectionStringName = "LibraryDB";
-
-if (!string.IsNullOrEmpty(docker_env))
-{
-	// Мы внутри Docker
-	// Отключаем использование HTTPS
-	builder.WebHost.ConfigureKestrel(serverOptions =>
-	{
-		serverOptions.ListenAnyIP(5000);  // HTTP порт
-	});
-	libConnectionStringName += "_Docker";
-}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -26,38 +11,21 @@ builder.Services.AddMediatR(config =>
 {
 	config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
-
 builder.Services.AddDbContext<LibraryDbContext>(options =>
 {
 	options.UseNpgsql(
-		builder.Configuration.GetConnectionString(libConnectionStringName)
+		builder.Configuration.GetConnectionString("LibraryDB")
 	);
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
-
-/*
- * 
- * {
-  "title": "The Great Gatsby",
-  "publishedYear": 1925,
-  "genre": "Fiction",
-  "authorId": 1,
-  "authorFullName": "F. Scott Fitzgerald",
-  "authorBirthday": "1896-09-24"
-}
-
- * 
- * */
